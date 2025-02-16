9016290905907d7fc97daa2fa2e443aefb0e047d
tester <polandtiara62@gmail.com>
2025-02-16 13:08:16 +0100

    lesson 06 - Матчеры (completed)

--- app/src/androidTest/java/org/wikipedia/homeworks/homework06/Matchers.kt
+++ app/src/androidTest/java/org/wikipedia/homeworks/homework06/Matchers.kt
@@ -3,6 +3,7 @@ package org.wikipedia.homeworks.homework06
 import org.hamcrest.Description
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.TypeSafeMatcher
+import org.hamcrest.core.AllOf.allOf
 
 enum class Color {
     RED, BLUE, GREEN, YELLOW, BLACK, WHITE
@@ -34,12 +35,38 @@ fun size(min: Float, max: Float): SizeValidationMatcher {
 }
 
 //Проверка количества углов
+//Для фигур с 3 и более сторонами количество углов совпадает с количеством сторон.
+//Для фигур с 1 или 2 сторонами (линий) углы отсутствуют (значение углов = 0).
+
+
+class AngleChecker(
+//    private val angleCount: Int
+) : TypeSafeMatcher<Shape>() {
+    override fun describeTo(description: Description) {
+        description.appendText("")
+    }
+
+    override fun matchesSafely(item: Shape): Boolean {
+        if (item.sideCount in 1..2) {
+            return true
+        }
+        if (item.sideCount >= 3) {
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+fun angle(): AngleChecker {
+    return AngleChecker()
+}
 
 //Проверка на чётное количество сторон
 
 class EvenSideCount() : TypeSafeMatcher<Shape>() {
     override fun describeTo(description: Description) {
-        description.appendText("the side count is even")
+        description.appendText("Shape side count should be even")
     }
 
     override fun matchesSafely(item: Shape): Boolean {
@@ -81,15 +108,58 @@ class NegativeSideCount() : TypeSafeMatcher<Shape>() {
     }
 }
 
-fun negCount() : NegativeSideCount {
+fun negCount(): NegativeSideCount {
     return NegativeSideCount()
+}
+
+//Проверка цвета фигуры
+
+class ColorChecker(
+    private val expectedColor: Color
+) : TypeSafeMatcher<Shape>() {
+    override fun describeTo(description: Description) {
+        description.appendText("Shape color should be ${expectedColor}")
+    }
+
+    override fun matchesSafely(item: Shape): Boolean {
+        return item.color == expectedColor
+    }
+}
+
+fun color(expectedColor: Color): ColorChecker {
+    return ColorChecker(expectedColor)
 }
 
 
 fun main() {
-    val shape1 = Shape(6.1f, -6, Color.RED)
+
+
+    val shape1 = Shape(6.1f, 6, Color.RED)
 //    assertThat(shape1, size(5.1f,100f))
     assertThat(shape1, even())
     assertThat(shape1, negLength())
     assertThat(shape1, negCount())
+    assertThat(shape1, color(Color.BLUE))
+
+    val shapes = listOf(
+        Shape(10f, 3, Color.RED), Shape(5f, 4, Color.BLUE), Shape(7f, 2, Color.GREEN),
+        Shape(0.5f, 1, Color.YELLOW), Shape(-3f, 5, Color.BLACK), Shape(8f, -2, Color.WHITE),
+        Shape(12f, 6, Color.RED), Shape(15f, 8, Color.BLUE), Shape(20f, 4, Color.GREEN),
+        Shape(9f, 5, Color.YELLOW), Shape(2f, 3, Color.BLACK), Shape(11f, 7, Color.WHITE),
+        Shape(6f, 10, Color.RED), Shape(3f, 2, Color.BLUE), Shape(4f, 1, Color.GREEN),
+        Shape(25f, 12, Color.YELLOW), Shape(30f, 14, Color.BLACK), Shape(35f, 16, Color.WHITE),
+        Shape(40f, 18, Color.RED), Shape(50f, 20, Color.BLUE)
+    )
+
+    val filteredShapes = shapes.filter { item ->
+        allOf(
+            size(0.1f,100f),
+            even(),
+            negLength(),
+            negCount(),
+            color(Color.BLUE)
+        ).matches(item)
+    }
+
+    println(filteredShapes)
 }
