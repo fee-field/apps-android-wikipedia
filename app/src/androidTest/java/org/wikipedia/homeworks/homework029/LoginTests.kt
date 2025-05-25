29df77d5775cb9db966bd55b9a6f73b5d2f5058f
tester <polandtiara62@gmail.com>
2025-05-25 20:27:55 +0200

    lesson №31 - Dockerfile

--- app/src/androidTest/java/org/wikipedia/homeworks/homework029/LoginTests.kt
+++ app/src/androidTest/java/org/wikipedia/homeworks/homework029/LoginTests.kt
@@ -1,6 +1,8 @@
 package org.wikipedia.homeworks.homework029
 
 import androidx.test.ext.junit.rules.ActivityScenarioRule
+import com.kaspersky.components.alluresupport.withForcedAllureSupport
+import com.kaspersky.kaspresso.kaspresso.Kaspresso
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
 import org.junit.Rule
 import org.junit.Test
@@ -9,7 +11,7 @@ import org.wikipedia.homeworks.homework022.ExploreScreenNew
 import org.wikipedia.homeworks.homework08.OnboardingScreen
 import org.wikipedia.main.MainActivity
 
-class LoginTests : TestCase() {
+class LoginTests : TestCase(kaspressoBuilder = Kaspresso.Builder.withForcedAllureSupport()) {
 
     @get:Rule
     val activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
