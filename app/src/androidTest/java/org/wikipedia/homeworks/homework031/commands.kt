d6695dd0381c7a6f78dbb09d3c0e58bb5b6c81fc
tester <polandtiara62@gmail.com>
2025-05-25 20:48:23 +0200

    lesson №31 - Dockerfile

---
+++ app/src/androidTest/java/org/wikipedia/homeworks/homework031/commands.kt
@@ -0,0 +1,13 @@
+package org.wikipedia.homeworks.homework031
+
+//docker build -t my-hw-image:1.0.2 .
+//docker run -it my-first-image:1.0.2 bash
+//cd ~
+//git clone git@github.com:fee-field/apps-android-wikipedia.git
+//cd apps-android-Wikipedia/
+//./gradlew assembleAlphaDebug
+//echo "HomeWorkExample=testtest" > local.properties
+//cat local.properties
+//echo "blaLogin=TestPassword" >
+//
+//PS C:\Users\> docker exec -it competent_roentgen bash
